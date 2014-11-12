a834c9b8ef8049ad325ec25595a7876a39da99b3
Thorsten Hans <thorsten.hans@gmail.com>
2014-11-12 03:27:43 +0000

    changed filename casing

---
+++ TodoSPA/App/Scripts/app.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalProvider.init(
+        {
+            tenant: 'dotnet-rocks.com',
+            clientId: '275aad91-14ba-4b53-b8b0-17706dc13887',
+            extraQueryParameter: 'nux=1'
+        },
+        $httpProvider
+        );
+   
+}]);
