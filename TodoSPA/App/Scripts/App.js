a022a746d8764a4a753fd92c84a9cf87e4b00121
Thorsten Hans <thorsten.hans@gmail.com>
2014-11-11 18:31:18 -0800

    Refactored to some real AngularJS

--- TodoSPA/App/Scripts/App.js
+++ TodoSPA/App/Scripts/App.js
@@ -1,27 +1,26 @@
 ﻿'use strict';
-var app = angular.module('TodoSPA', ['ngRoute','AdalAngular']);
-// version 1
-app.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalAuthenticationServiceProvider) {
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
 
     $routeProvider.when("/Home", {
-        controller: "HomeController",
+        controller: "homeCtrl",
         templateUrl: "/App/Views/Home.html",
     }).when("/TodoList", {
-        controller: "TodoListController",
+        controller: "todoListCtrl",
         templateUrl: "/App/Views/TodoList.html",
         requireADLogin: true,
     }).when("/UserData", {
-        controller: "UserDataController",
+        controller: "userDataCtrl",
         templateUrl: "/App/Views/UserData.html",
     }).otherwise({ redirectTo: "/Home" });
 
-    adalAuthenticationServiceProvider.init(
+    adalProvider.init(
         {
             tenant: 'Enter your tenant name here e.g. contoso.onmicrosoft.com',
             clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
             extraQueryParameter: 'nux=1'
         },
-        $httpProvider   // pass http provider to inject request interceptor to attach tokens
+        $httpProvider
         );
    
 }]);
---
+++ TodoSPA/App/Scripts/homeCtrl.js
@@ -0,0 +1,12 @@
+﻿angular.module('')
+.controller('homeCtrl', ['$scope', 'adalAuthenticationService','$location', function ($scope, adalService, $location) {
+    $scope.login = function () {
+        adalService.login();
+    };
+    $scope.logout = function () {
+        adalService.logOut();
+    };
+    $scope.isActive = function (viewLocation) {        
+        return viewLocation === $location.path();
+    };
+}]);
