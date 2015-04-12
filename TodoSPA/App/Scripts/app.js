8bb527106b937f18ccb03d47c731d9d54e36562e
dstrockis <dastrock@microsoft.com>
2015-04-11 22:26:56 -0700

    Switching to new AAD endpoint

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -16,6 +16,7 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
 
     adalProvider.init(
         {
+            instance: 'https://login.microsoftonline.com/', 
             tenant: 'Enter your tenant name here e.g. contoso.onmicrosoft.com',
             clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
             extraQueryParameter: 'nux=1',
