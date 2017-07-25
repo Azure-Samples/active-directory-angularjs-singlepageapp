bb3f920574831045c89a4076cea19f6392bcf11d
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-07-25 14:06:10 +0200

    Refreshing the sample: - Updating for VS 2017 - Updating the sample to use recent versions of the NuGet packages for OWin, EntityFramework, ASP.NET - Removing nux=1. - Updating the connection string for the EntityFramework database.

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 angular.module('todoApp', ['ngRoute','AdalAngular'])
 .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
 
@@ -19,7 +19,7 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
             instance: 'https://login.microsoftonline.com/', 
             tenant: 'Enter your tenant name here e.g. contoso.onmicrosoft.com',
             clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
-            extraQueryParameter: 'nux=1',
+            // extraQueryParameter: 'nux=1',
             //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
         },
         $httpProvider
