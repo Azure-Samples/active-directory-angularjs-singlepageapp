528517865b62a353ff06ed24728c6b25ee054bfa
Daniel Dobalian <ddobalian@gmail.com>
2017-07-25 11:00:40 -0700

    Removed nux

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -20,7 +20,6 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
             tenant: 'Enter your tenant name here e.g. contoso.onmicrosoft.com',
             clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
             popUp: false,
-            // extraQueryParameter: 'nux=1',
             //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
         },
         $httpProvider
